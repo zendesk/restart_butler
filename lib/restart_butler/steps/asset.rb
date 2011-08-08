b44428934b08c975be986733ed5eecc3c26aafa6
Michal Bugno <michal.bugno@gmail.com>
2011-08-08 15:42:43 +0200

    Assets are rsynced to destination

--- lib/restart_butler/steps/asset.rb
+++ lib/restart_butler/steps/asset.rb
@@ -1,9 +1,14 @@
 class RestartButler::Steps::Asset < RestartButler::Steps::Base
   def execute
     butler.run_command("./script/assets")
+    butler.run_command("rsync -avr public/ #{opts[:destination]}")
   end
 
   def should_trigger?
     butler.changed_dir?('public') or butler.changed_file?('Assetfile')
+  end
+
+  def option_keys
+    [:destination]
   end
 end
