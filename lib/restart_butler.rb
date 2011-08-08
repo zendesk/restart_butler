15a5e1fce432ce58b9998637ada0f56e8237d92c
Michal Bugno <michal.bugno@gmail.com>
2011-08-08 15:27:06 +0200

    Autoload all steps

--- lib/restart_butler.rb
+++ lib/restart_butler.rb
@@ -3,6 +3,11 @@ module RestartButler
 
   module Steps
     autoload "Base", "restart_butler/steps/base"
+    autoload "Asset", "restart_butler/steps/asset"
     autoload "Bundle", "restart_butler/steps/bundle"
+    autoload "Cron", "restart_butler/steps/cron"
+    autoload "Delayed", "restart_butler/steps/delayed"
+    autoload "God", "restart_butler/steps/god"
+    autoload "Migrate", "restart_butler/steps/migrate"
   end
 end
