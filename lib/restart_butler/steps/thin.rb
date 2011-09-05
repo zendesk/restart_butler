835f73431974b7f499608020d94c8b21b5c226f9
Michal Bugno <michal.bugno@gmail.com>
2011-09-05 15:23:54 +0200

    Fixing rails_env ref from config

--- lib/restart_butler/steps/thin.rb
+++ lib/restart_butler/steps/thin.rb
@@ -1,7 +1,7 @@
 class RestartButler::Steps::Thin < RestartButler::Steps::Base
   def execute
-    butler.run_command("./bin/thin stop     -c #{butler.root_dir} -P #{butler.root_dir}/tmp/pids/thin.#{opts[:port]}.pid -p #{opts[:port]} -e #{butler.opts["RAILS_ENV"]}")
-    butler.run_command("./bin/thin start -d -c #{butler.root_dir} -P #{butler.root_dir}/tmp/pids/thin.#{opts[:port]}.pid -p #{opts[:port]} -e #{butler.opts["RAILS_ENV"]}")
+    butler.run_command("./bin/thin stop     -c #{butler.root_dir} -P #{butler.root_dir}/tmp/pids/thin.#{opts[:port]}.pid -p #{opts[:port]} -e #{butler.opts[:env]["RAILS_ENV"]}")
+    butler.run_command("./bin/thin start -d -c #{butler.root_dir} -P #{butler.root_dir}/tmp/pids/thin.#{opts[:port]}.pid -p #{opts[:port]} -e #{butler.opts[:env]["RAILS_ENV"]}")
   end
 
   def should_trigger?
