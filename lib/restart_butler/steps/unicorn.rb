053c9bd9121835cfe9a8ee29ce996e12f49bbb69
Michal Bugno <michal.bugno@gmail.com>
2011-11-29 17:47:41 +0100

    Version 0.0.3

--- lib/restart_butler/steps/unicorn.rb
+++ lib/restart_butler/steps/unicorn.rb
@@ -1,9 +1,9 @@
 class RestartButler::Steps::Unicorn < RestartButler::Steps::Base
   def execute
     if File.exists?("#{butler.root_dir}/log/unicorn.pid")
-      run "rvm use 1.9.3-p0 && kill -USR2 `cat #{butler.root_dir}log/unicorn.pid`"
+      butler.run_command("rvm use 1.9.3-p0 && kill -USR2 `cat #{butler.root_dir}log/unicorn.pid`")
     else
-      run "rvm use 1.9.3-p0 && ./bin/unicorn -c #{butler.root_dir}/config/unicorn.rb -E #{butler.opts[:env]["RAILS_ENV"]} -D -p #{opts[:port]}"
+      butler.run_command("rvm use 1.9.3-p0 && ./bin/unicorn -c #{butler.root_dir}/config/unicorn.rb -E #{butler.opts[:env]["RAILS_ENV"]} -D -p #{opts[:port]}")
     end
   end
 
